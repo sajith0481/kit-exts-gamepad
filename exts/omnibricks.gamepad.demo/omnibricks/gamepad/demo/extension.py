16169cfc20cb2ef10b67db559d05d8f07eb23b97
SAJITH JOSEPH <sajith0481@gmail.com>
2023-11-02 21:27:36 +0100

    move_camera changed to move_drone

--- exts/omnibricks.gamepad.demo/omnibricks/gamepad/demo/extension.py
+++ exts/omnibricks.gamepad.demo/omnibricks/gamepad/demo/extension.py
@@ -29,7 +29,7 @@ class OmnibricksGamepadDemoExtension(omni.ext.IExt):
 
         # get camera
         self.stage = omni.usd.get_context().get_stage()
-        self.prim = self.stage.GetPrimAtPath("/World/Camera")
+        self.prim = self.stage.GetPrimAtPath("/World/quadrotor")
         self.xform = UsdGeom.Xformable(self.prim)
         
 
@@ -56,7 +56,7 @@ class OmnibricksGamepadDemoExtension(omni.ext.IExt):
                 with ui.HStack():
                     ui.Button("Add", clicked_fn=on_click)
                     ui.Button("Reset", clicked_fn=on_reset)
-                    ui.Button("Move Camera", clicked_fn=self.move_camera)
+                    ui.Button("Move Camera", clicked_fn=self.move_drone)
 
     # Handling Gamepad Events for a Vehicle
     def on_gamepad_event(self, event: GamepadEvent):
@@ -73,30 +73,42 @@ class OmnibricksGamepadDemoExtension(omni.ext.IExt):
 
         elif event.input == GamepadInput.A:
             # self._dpad_count = 0
-            self.move_camera()
+            self.move_drone()
         
         self.gamepad_info.text = f"gamepad: {self._dpad_count}"
 
-    def move_camera(self):
+    def move_drone(self):
         local_transformation: Gf.Matrix4d = self.xform.GetLocalTransformation()
-        # Apply the local matrix to the start and end points of the camera's default forward vector (-Z)
+
+        # Define the drone's forward direction (adjust this based on the drone's orientation)
+        drone_forward_vector = Gf.Vec4d(1,0,0,1)  # Change this to match the drone's forward direction
+
+        # Apply the local matrix to the start and end points of the drone's forward vector
         a: Gf.Vec4d = Gf.Vec4d(0,0,0,1) * local_transformation
-        b: Gf.Vec4d = Gf.Vec4d(0,0,-1,1) * local_transformation
-        # Get the vector between those two points to get the camera's current forward vector
-        cam_fwd_vec = b-a
+        b: Gf.Vec4d = drone_forward_vector * local_transformation
+
+        # Get the vector between those two points to get the drone's current forward vector
+        drone_fwd_vec = b-a
+
         # Convert to Vec3 and then normalize to get unit vector
-        cam_fwd_unit_vec = Gf.Vec3d(cam_fwd_vec[:3]).GetNormalized()
+        drone_fwd_unit_vec = Gf.Vec3d(drone_fwd_vec[:3]).GetNormalized()
+
         # Multiply the forward direction vector with how far forward you want to move
-        forward_step = cam_fwd_unit_vec * 100
+        forward_step = drone_fwd_unit_vec * 1
+
         # Create a new matrix with the translation that you want to perform
         offset_mat = Gf.Matrix4d()
         offset_mat.SetTranslate(forward_step)
+
         # Apply the translation to the current local transform
         new_transform = local_transformation * offset_mat
+
         # Extract the new translation
         translate: Gf.Vec3d = new_transform.ExtractTranslation()
+
         # Update the attribute
         self.prim.GetAttribute("xformOp:translate").Set(translate)
+
 
     def on_shutdown(self):
         self.input.unsubscribe_to_gamepad_events(self.gamepad, self.gamepad_event_sub)
